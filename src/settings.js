1d27523e903607d568896dbf89a7854d99fd51f5
sylor <sylor8@gmail.com>
2025-10-30 17:02:35 +0800

    fix

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 const setting = {
   title: 'Batch Burn Tokens, Reclaim Accounts On Solana',
-  ca: "So11111111111111111111111111111111111111112",
+  ca: "FTJMfBjm1qu6Gk6GSSqD2y4854YXNBqMzxBSdBPCpump",
   x: "https://x.com/i/communities/1939497856274186406",
   github: "https://github.com/Sylor-huang/ashsol",
   info: "Pure JS front-end framework, open source code, no risk.",
