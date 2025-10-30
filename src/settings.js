e4bb980fc6d790e14af7639c235412934b1fffcf
sylor <sylor8@gmail.com>
2025-10-30 15:19:59 +0800

    update setting

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,8 @@
 const setting = {
   title: 'Batch Burn Tokens, Reclaim Accounts On Solana',
   ca: "So11111111111111111111111111111111111111112",
-  x: "https://x.com/ChainLuckyLotto",
-  github: "https://github.com/Lucky-Lotto/contracts",
+  x: "https://x.com/i/communities/1939497856274186406",
+  github: "https://github.com/Sylor-huang/ashsol",
   info: "Pure JS front-end framework, open source code, no risk.",
   freeRpc: "https://solana-mainnet.g.alchemy.com/v2/lF1uLnJkjP95iFQGASKRGLjELbCbdxmV",
   theme: "light",
